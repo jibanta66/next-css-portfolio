a73b8f73a22d25473f9d8d67366b82a1232b56e1
Jibanta <63874888+jibanta66@users.noreply.github.com>
2022-10-10 05:39:10 +0700

    g

--- pages/index.js
+++ pages/index.js
@@ -83,8 +83,8 @@ export default function Home() {
                 </motion.button>
             </div>
             
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-70 md:w-70">
-              <Image src={deved} width={100} height={100} layout="fill" objectFit="cover" />
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={deved} layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>
